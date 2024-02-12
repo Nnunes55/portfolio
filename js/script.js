ddec7a48be484fb46d07c45a8cd7aa7274bb5e14
Nathan Nunes <nathannunessp587@gmail.com>
2024-02-12 00:02:43 -0300

    atualizações

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 /* ============================== typing animation ============================ */
 var typed = new Typed(".typing",{
-    strings:["","Ciência da Computação","Front-End","Back-End","Salesforce"],
+    strings:["","Front-End","Back-End","Salesforce"],
     typeSpeed:100,
     BackSpeed:60,
     loop:true
